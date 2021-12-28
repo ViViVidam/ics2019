f81abec55bc9424fc206644f82d0f44e5cdecac7
tracer-ics2019 <tracer@njuics.org>
2021-12-27 22:20:02 -0800

    >  compile U201811329 xie Linux xie-MACHR-WX9 5.4.0-91-generic #102-Ubuntu SMP Fri Nov 5 16:31:28 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  22:20:02 up  4:30,  0 users,  load average: 1.51, 1.39, 1.10 3809569119a19ac9a2bd4fe301037376e534076

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -39,8 +39,9 @@ static int cmd_q(char *args) {
 static int cmd_help(char *args);
 
 static int cmd_si(char *args){
+  printf("%s\n",args);
   char *arg = strtok(args, " ");
-  printf("%s %s",arg,args);
+  printf("%s %s\n",arg,args);
   //int count = atoi(arg);
   //printf("%s %d",arg,count);
   //cpu_exec(count);
