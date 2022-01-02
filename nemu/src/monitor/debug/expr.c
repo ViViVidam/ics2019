4327c4ca37ed375a456199131b6996d8fe985777
tracer-ics2019 <tracer@njuics.org>
2022-01-01 19:46:52 -0800

    >  compile U201811329 xie Linux xie-MACHR-WX9 5.4.0-91-generic #102-Ubuntu SMP Fri Nov 5 16:31:28 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  19:46:52 up  1:12,  0 users,  load average: 0.41, 0.33, 0.27 2251e228e2773e33f4f1682f97cca5c011a371ed

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -225,7 +225,7 @@ uint32_t expr(char *e, bool *success) {
     int x = statestack[statepointer-1];
     int y = getindex(tokens[i].type);
     int action = reg_expr[x][y];
-    printf("action: %d\n",action);
+    printf("(%d %d)action: %d\n",x,y,action);
     if(action>RULE){
       action = action - RULE;
       switch(action){
