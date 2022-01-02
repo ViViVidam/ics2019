add582b9f76300d98ef71d17d1a5cf9876080288
tracer-ics2019 <tracer@njuics.org>
2022-01-01 22:04:22 -0800

    >  compile U201811329 xie Linux xie-MACHR-WX9 5.4.0-91-generic #102-Ubuntu SMP Fri Nov 5 16:31:28 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  22:04:22 up 31 min,  0 users,  load average: 0.39, 0.29, 0.34 f05f6202a2e4b9f1ff876a80ce56c25c31188f0

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -14,7 +14,7 @@ void isa_reg_display() {
 }
 
 uint32_t isa_reg_str2val(const char *s, bool *success) {
-  printf("reg:%s %b\n",s,success);
+  printf("reg:%s %d\n",s,*success);
   for(int i=0;i<32;i++){
     printf("%s\n",reg_name(i,32));
     if(strcmp(s,reg_name(i,32))==0){
