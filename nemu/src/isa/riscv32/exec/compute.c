8166448e1990675b0d58bfac8efd764fa232c0eb
tracer-ics2019 <tracer@njuics.org>
2022-01-04 14:32:08 -0800

    >  compile U201811329 xie Linux xie-MACHR-WX9 5.4.0-92-generic #103-Ubuntu SMP Fri Nov 26 16:13:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  14:32:08 up  3:05,  0 users,  load average: 0.45, 0.33, 0.37 7cbd76c29132f6aed73440a5bc86c44fe37d16c

--- nemu/src/isa/riscv32/exec/compute.c
+++ nemu/src/isa/riscv32/exec/compute.c
@@ -7,13 +7,14 @@ make_EHelper(lui) {
 }
 
 make_EHelper(auipc) {
-  int32_t tmp = cpu.pc + id_src->val;
+  int tmp = cpu.pc + id_src->val;
   printf("auipc %d %x %x\n",id_dest->reg,cpu.pc,tmp);
   rtl_sr(id_dest->reg,&tmp,4);
 }
 
 make_EHelper(addi){
   uint32_t tmp = 0;
-  rtl_add(id_dest->reg,&id_src->val,&id_src2->val);
+  rtl_add(&id_dest->val,&id_src->val,&id_src2->val);
+  rtl_sr(id_dest->reg,&id_dest->val,4);
   printf("addi %x %x %d\n",id_dest->reg,id_src->val,id_src2->val);
 }
