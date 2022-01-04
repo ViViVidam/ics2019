0f9525ac5acaa5f2513f46f6f89698ae1f5bc764
tracer-ics2019 <tracer@njuics.org>
2022-01-04 13:47:22 -0800

    >  compile U201811329 xie Linux xie-MACHR-WX9 5.4.0-92-generic #103-Ubuntu SMP Fri Nov 26 16:13:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  13:47:22 up  2:20,  0 users,  load average: 0.50, 0.47, 0.40 d947e229a2cd32d41e59f5c6530f35eef022224a

--- nemu/src/isa/riscv32/exec/compute.c
+++ nemu/src/isa/riscv32/exec/compute.c
@@ -13,6 +13,6 @@ make_EHelper(auipc) {
 }
 
 make_EHelper(addi){
-  printf("exec_addi called\n");
   rtl_add(&id_dest->reg,&id_src->val,&id_src2->val);
+  printf("addi %x %x %x\n",id_dest->reg,id_src->val,id_src2->val);
 }
