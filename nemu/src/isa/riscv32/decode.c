b988d3d92cac5e640cc495b3007fd273dca8cb7e
tracer-ics2019 <tracer@njuics.org>
2022-01-04 14:00:22 -0800

    >  compile U201811329 xie Linux xie-MACHR-WX9 5.4.0-92-generic #103-Ubuntu SMP Fri Nov 26 16:13:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  14:00:22 up  2:33,  0 users,  load average: 0.39, 0.43, 0.40 4d93e1e2fe4e5b2fdf5cd568e1c11be71a85641

--- nemu/src/isa/riscv32/decode.c
+++ nemu/src/isa/riscv32/decode.c
@@ -89,8 +89,7 @@ make_DHelper(lui){
 make_DHelper(addi){
   printf("addi called\n");
   decode_op_r(id_src,decinfo.isa.instr.rs1,true);
-  decode_op_i(id_src2,decinfo.isa.instr.simm11_0,false);
-
+  decode_op_i(id_src2,decinfo.isa.instr.simm11_0,true);
   decode_op_r(id_dest,decinfo.isa.instr.rd,false);
   //print_Dop(id_src->str,OP_STR_SIZE,"0x%x",defin)
 }
