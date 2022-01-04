dfd867839fa047a57e52926b243a6e5f3a4b02dd
tracer-ics2019 <tracer@njuics.org>
2022-01-04 14:22:39 -0800

    >  compile U201811329 xie Linux xie-MACHR-WX9 5.4.0-92-generic #103-Ubuntu SMP Fri Nov 26 16:13:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  14:22:39 up  2:55,  0 users,  load average: 0.59, 0.54, 0.48 375ad187841d0af9bcc295b97e139b8b3290f395

--- nemu/src/isa/riscv32/exec/control.c
+++ nemu/src/isa/riscv32/exec/control.c
@@ -1,16 +1,18 @@
 #include "cpu/exec.h"
 make_EHelper(jal){
-  rtl_sr(id_dest->reg,&(cpu.pc),4);
+  uint32_t nextpc = cpu.pc + 4;
+  rtl_sr(id_dest->reg,&(nextpc),4);
 	cpu.pc += id_src->val;
 	decinfo.jmp_pc = cpu.pc;
   printf("jal pc:%x\n",cpu.pc);
 }
 
 make_EHelper(jalr){
+  uint32_t nextpc = cpu.pc + 4;
   int32_t tmp = id_src->val + id_src2->val;
   tmp = tmp >> 1;
   tmp = tmp << 1;
-  rtl_sr(id_dest->reg,&(cpu.pc),4);
+  rtl_sr(id_dest->reg,&(nextpc),4);
   cpu.pc = tmp;
   printf("jalr pc:%x %x %x\n",cpu.pc,id_src->val,id_src2->val);
 }
