960ba25d5423e0fd0265ba285fced3fae87f3070
tracer-ics2019 <tracer@njuics.org>
2022-01-04 13:00:05 -0800

    >  compile U201811329 xie Linux xie-MACHR-WX9 5.4.0-92-generic #103-Ubuntu SMP Fri Nov 26 16:13:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  13:00:05 up  1:33,  0 users,  load average: 0.55, 0.41, 0.36 ed289e30535d45aeaf36bb30e41118b32456557f

--- nemu/src/isa/riscv32/decode.c
+++ nemu/src/isa/riscv32/decode.c
@@ -62,7 +62,7 @@ make_DHelper(st) {
 make_DHelper(jal){
   int32_t offset = decinfo.isa.instr.imm31_12;
   offset = offset << 12;
-  offset = offset >> 12;
+  offset = offset >> 14;
   decinfo.is_jmp  =true;
   decode_op_i(id_src,offset,true);
   decode_op_r(id_dest,decinfo.isa.instr.rd,false);
