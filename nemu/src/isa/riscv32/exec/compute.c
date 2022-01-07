a40f489f9c8b55334eb5a13a1c623f4b532dc865
tracer-ics2019 <tracer@njuics.org>
2022-01-06 18:27:03 -0800

    >  compile U201811329 xie Linux xie-MACHR-WX9 5.4.0-92-generic #103-Ubuntu SMP Fri Nov 26 16:13:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  18:27:03 up  2:18,  0 users,  load average: 0.18, 0.34, 0.42 99616a90c3f122cf47667c5a6ca2f2f0364bd6d9

--- nemu/src/isa/riscv32/exec/compute.c
+++ nemu/src/isa/riscv32/exec/compute.c
@@ -153,8 +153,8 @@ make_EHelper(rem){
 }
 
 make_EHelper(mulh){
-  int64_t val1 = id_src->val;
-  int64_t val2 = id_src2->val;
+  int32_t val1 = id_src->val;
+  int32_t val2 = id_src2->val;
   int64_t results = val1*val2;
   rtl_mul_hi(&id_dest->val,&id_src->val,&id_src2->val);
   printf("%ld %lx\n",results,results);
