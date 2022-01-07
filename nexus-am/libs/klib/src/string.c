5e184c573023ec7e3a059594111078b581554cfb
tracer-ics2019 <tracer@njuics.org>
2022-01-06 19:38:52 -0800

    >  run U201811329 xie Linux xie-MACHR-WX9 5.4.0-92-generic #103-Ubuntu SMP Fri Nov 26 16:13:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  19:38:52 up  3:30,  0 users,  load average: 0.37, 0.47, 0.45 4a96c33add857d278dbd418b7b20de3860991ee5

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -3,34 +3,100 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  return 0;
+  size_t i = 0;
+  while(s[i]!='\0'){
+    i++;
+  }
+  return i;
 }
 
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+  //biggest type to held any data
+  size_t pointer = 0;
+  while(*(src+pointer)){
+    *(dst+pointer) = *(src+pointer);
+  }
+  *(dst+pointer)='\0';
+  return dst;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  return NULL;
+  size_t i;
+  for (i = 0; i < n && src[i] != '\0'; i++)
+     dst[i] = src[i];
+  for ( ; i < n; i++)
+     dst[i] = '\0';
+
+  return dst;
 }
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+  size_t pointer = 0;
+  while(*(dst+pointer)!='\0'){
+    pointer++;
+  }
+  while(*src!='\0'){
+    *(dst+pointer) = *src;
+    src++;
+    pointer++;
+  }
+  *(dst+pointer)='\0';
+  return dst;
 }
 
 int strcmp(const char* s1, const char* s2) {
-  return 0;
+  size_t i = 0;
+  uint8_t tmp = 0;
+  while(*(s1+i)!='\0'){
+    if(*(s2+i)==0){
+      return 1;
+    }
+    tmp = *(s1+i)-*(s2+i);
+    if(tmp>0){
+      return 1;
+    }
+    else if(tmp<0){
+      return -1;
+    }
+  }
+  if(*(s2+i)!='\0')
+    return -1;
+  else
+    return 0;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-  return 0;
+  size_t i = 0;
+  uint8_t tmp = 0;
+  for(i=0;i<n;i++){
+
+    if(*(s2+i)==0){
+      return 1;
+    }
+    tmp = *(s1+i)-*(s2+i);
+    if(tmp>0){
+      return 1;
+    }
+    else if(tmp<0){
+      return -1;
+    }
+  }
+  if(*(s2+i)!='\0')
+    return -1;
+  else
+    return 0;
 }
 
 void* memset(void* v,int c,size_t n) {
+  /*size_t i = 0;
+  for(i=0;i<n;i++){
+    isa_vaddr_write(v, c, 1);
+  }*/
   return NULL;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
+
   return NULL;
 }
 
