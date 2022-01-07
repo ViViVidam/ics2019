2ed3684b8666f870fcfc6b396c01477f2a04d937
tracer-ics2019 <tracer@njuics.org>
2022-01-07 11:06:15 -0800

    >  compile U201811329 xie Linux xie-MACHR-WX9 5.4.0-92-generic #103-Ubuntu SMP Fri Nov 26 16:13:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  11:06:15 up 44 min,  0 users,  load average: 0.51, 0.26, 0.22 7a2d996dda8b03098addbfc15a59697d84963c5

--- nemu/src/isa/riscv32/diff-test.c
+++ nemu/src/isa/riscv32/diff-test.c
@@ -1,8 +1,20 @@
 #include "nemu.h"
 #include "monitor/diff-test.h"
 
+const char *regsname[] = {
+  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
+  "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
+  "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
+  "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
+};
+
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  int i = 0;
+  for(i=0;i<32;i++){
+    if(ref_r->gpr[i]._32!=reg_l(i))
+      return false;
+  }
+  return true;
 }
 
 void isa_difftest_attach(void) {
