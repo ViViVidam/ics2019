e76eeb41dea87c4fbfaca14cc1f2bfe7223d4f1e
tracer-ics2019 <tracer@njuics.org>
2022-01-06 20:10:46 -0800

    >  run U201811329 xie Linux xie-MACHR-WX9 5.4.0-92-generic #103-Ubuntu SMP Fri Nov 26 16:13:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  20:10:46 up  4:02,  0 users,  load average: 0.81, 0.59, 0.51 6ff5a5e61444ececde9e21c4f65315ccecd0c116

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -68,8 +68,7 @@ int strcmp(const char* s1, const char* s2) {
 int strncmp(const char* s1, const char* s2, size_t n) {
   size_t i = 0;
   uint8_t tmp = 0;
-  for(i=0;i<n;i++){
-    printf("5\n");
+  for(i=0;i<n&&s1[i]!='\0';i++){
     if(*(s2+i)==0){
       return 1;
     }
@@ -88,19 +87,37 @@ int strncmp(const char* s1, const char* s2, size_t n) {
 }
 
 void* memset(void* v,int c,size_t n) {
-  /*size_t i = 0;
-  for(i=0;i<n;i++){
-    isa_vaddr_write(v, c, 1);
-  }*/
-  return NULL;
+  char* p=v;
+  while(n--)
+  {
+      *p++ = (unsigned char)c;
+  }
+  return v;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-
-  return NULL;
+  size_t i = 0;
+  char* p = out;
+  char* t = in;
+  for(i=0;i<n;i++){
+    p[i] = t[i];
+  }
+  return out;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
+  char* p = s1;
+  char* t = s2;
+  char tmp = 0;
+  while(n--){
+    tmp = *p-*t;
+    if(tmp>0){
+      return 1;
+    }
+    else if(tmp<0){
+      return -1;
+    }
+  }
   return 0;
 }
 
