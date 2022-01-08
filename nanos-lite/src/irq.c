d6b889ac100e8ea1b3a18709bff5851c5659c505
tracer-ics2019 <tracer@njuics.org>
2022-01-08 15:28:29 -0800

    >  run U201811329 xie Linux xie-MACHR-WX9 5.4.0-92-generic #103-Ubuntu SMP Fri Nov 26 16:13:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:28:29 up 20 min,  0 users,  load average: 0.51, 0.40, 0.32 872de04d15cd721cf9631384fc40ced65351b996

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -4,6 +4,9 @@ static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
     case _EVENT_YIELD: printf("YIELD \n");
     break;
+    case _EVENT_ERROR: 
+      printf("failure in cause id %d\n",c->cause);
+      break;
     case _EVENT_SYSCALL: 
       do_syscall(c);
     break;
