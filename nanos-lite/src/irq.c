551e0ac78591de3e2d2e4f412c3f8236f49f8020
tracer-ics2019 <tracer@njuics.org>
2022-01-08 13:39:08 -0800

    >  run U201811329 xie Linux xie-MACHR-WX9 5.4.0-92-generic #103-Ubuntu SMP Fri Nov 26 16:13:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  13:39:08 up  1:24,  0 users,  load average: 0.14, 0.27, 0.32 aa9d75fe10a12eaaf6bad3c61a0e93874881c79d

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,6 +2,8 @@
 
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
+    case 1: printf("event ID = 1\n");
+    break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
