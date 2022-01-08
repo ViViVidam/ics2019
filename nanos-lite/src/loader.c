0398c7900eed8f32197f01bfe42aa9a377ea95bc
ViVidam <1325507519@qq.com>
2022-01-08 13:30:04 -0800

    local

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -19,18 +19,11 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf32_Off segmentoffset = 0;
   Elf32_Off segcontent_off = 0;
   ramdisk_read(&header, 0, sizeof(Elf32_Ehdr));
-  printf("%x\n",&ramdisk_start);
-  printf("num %d\n",header.e_phnum);
-  printf("header %d %d %d\n",header.e_phoff,header.e_ehsize,header.e_phentsize);
   segmentoffset = header.e_phoff;
   for(int i=0;i<header.e_phnum;i++){
-    printf("load header from: %x\n",header.e_phoff+i*header.e_phentsize+&ramdisk_start);
     ramdisk_read(&segment,header.e_phoff+i*header.e_phentsize,header.e_phentsize);
-    printf("%d %d\n",segment.p_offset,segment.p_type);
     if(segment.p_type==PT_LOAD){
-      printf("pt load\n");
       memcpy(segment.p_vaddr,&ramdisk_start+segment.p_offset,segment.p_filesz);
-      printf("%x %d %x %x\n",segment.p_vaddr,segment.p_offset,segment.p_filesz,segment.p_memsz);
       //memset(segment.p_vaddr+segment.p_filesz,0,segment.p_memsz-segment.p_filesz);
     }
   }
@@ -47,25 +40,6 @@ void naive_uload(PCB *pcb, const char *filename) {
   }
   ((void(*)())entry) ();
 }
-//finished loader
-/*
-static uintptr_t loader(PCB *pcb, const char *filename) {
-  Elf_Ehdr head;
-  int fd=fs_open(filename,0,0);
-  fs_lseek(fd,0,SEEK_SET);
-  fs_read(fd,&head,sizeof(head));
-  for(int i=0;i<head.e_phnum;i++){
-    Elf_Phdr temp;
-    fs_lseek(fd,head.e_phoff+i*head.e_phentsize,SEEK_SET);
-    fs_read(fd,&temp,sizeof(temp));
-    if(temp.p_type==PT_LOAD){
-      fs_lseek(fd,temp.p_offset,SEEK_SET);
-      fs_read(fd,(void*)temp.p_vaddr,temp.p_filesz);
-      memset((void*)(temp.p_vaddr+temp.p_filesz),0,temp.p_memsz-temp.p_filesz);
-    }
-  }
-  return head.e_entry;
-}*/
 
 void context_kload(PCB *pcb, void *entry) {
   _Area stack;
