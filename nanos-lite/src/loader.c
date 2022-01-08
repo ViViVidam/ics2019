98000efb1ab14cabae3522cf7e133005e41e9b95
tracer-ics2019 <tracer@njuics.org>
2022-01-08 00:45:06 -0800

    >  run U201811329 xie Linux xie-MACHR-WX9 5.4.0-92-generic #103-Ubuntu SMP Fri Nov 26 16:13:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  00:45:06 up 43 min,  0 users,  load average: 0.27, 0.37, 0.35 8103518da2ec92a14658196fd6c3400ed2e8822a

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -22,7 +22,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   printf("num %d\n",header.e_phnum);
   segmentoffset = header.e_phoff;
   for(int i=0;i<header.e_phnum;i++){
-    ramdisk_read(&segment,header.e_phoff+i*header.e_phentsize,sizeof(segment));
+    ramdisk_read(&segment,header.e_phoff+i*header.e_phentsize,header.e_phentsize);
     if(segment.p_type==PT_LOAD){
       printf("pt load\n");
     }
