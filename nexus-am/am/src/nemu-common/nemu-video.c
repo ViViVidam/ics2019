3b6ef18e0e0125414b7f25c6422ebeca509e4396
tracer-ics2019 <tracer@njuics.org>
2022-01-07 22:10:00 -0800

    >  run U201811329 xie Linux xie-MACHR-WX9 5.4.0-92-generic #103-Ubuntu SMP Fri Nov 26 16:13:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  22:10:00 up  1:44,  0 users,  load average: 0.37, 0.48, 0.34 da7f8a47347211eb578d40e391c8fba0811e36d4

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -31,7 +31,7 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
       if (ctl->sync) {
         outl(SYNC_ADDR, 0);
       }
-      printf("(%d,%d) (%d,%d)\n",ctl->x,ctl->y,ctl->w,ctl->h);
+      //printf("(%d,%d) (%d,%d)\n",ctl->x,ctl->y,ctl->w,ctl->h);
       return size;
     }
   }
