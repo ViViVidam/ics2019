c9b93f7354c0b3ab80e2bf30b05d080deddd2949
tracer-ics2019 <tracer@njuics.org>
2022-01-07 23:17:07 -0800

    >  run U201811329 xie Linux xie-MACHR-WX9 5.4.0-92-generic #103-Ubuntu SMP Fri Nov 26 16:13:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  23:17:07 up  2:51,  0 users,  load average: 0.22, 0.24, 0.27 49974c96ea74f0d73c6b594d424297c909d0133

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -24,7 +24,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   segcontent_off = segment.p_offset;
   memsize = segment.p_memsz;
   vaddr = segment.p_vaddr;
-  printf("vaddr %d\n",vaddr);
+  printf("vaddr %d %d\n",memsize,vaddr);
   memset(vaddr,0,memsize);
   memcpy(vaddr,&ramdisk_start+segcontent_off+segmentoffset,memsize);
   printf("%d %d %d\n",segcontent_off,segmentoffset,&ramdisk_start);
