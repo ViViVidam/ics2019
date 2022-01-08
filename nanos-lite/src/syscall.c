ef85a2f29741019f18cbd875384d06c3844bdaf2
tracer-ics2019 <tracer@njuics.org>
2022-01-08 15:52:59 -0800

    >  run U201811329 xie Linux xie-MACHR-WX9 5.4.0-92-generic #103-Ubuntu SMP Fri Nov 26 16:13:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:52:59 up 45 min,  0 users,  load average: 0.35, 0.27, 0.30 e3a6abd279adfca1f65503d6f823f86a7c2fe765

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -6,15 +6,29 @@
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
   switch (a[0]) {
     case SYS_exit:
-      printf("%d\n",c->GPR2);
-      _halt(c->GPR2);
+      _halt(a[1]);
+      return 0;
     break;
     case SYS_yield:
       _yield();
       return 0;
+    case SYS_write:
+      if(a[1]==1||2){
+        int i = (int) a[3];
+        char* buffer = (char *)a[2];
+        while(i--){
+          _putc(*buffer);
+          buffer++;
+        }
+        return buffer-((char*)a[2]);
+      }
+      else
+        return -1;
     break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
