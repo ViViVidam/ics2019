8e118a37c197d6dd20a574dc9b51666b0c40b02f
tracer-ics2019 <tracer@njuics.org>
2022-01-08 13:41:16 -0800

    >  run U201811329 xie Linux xie-MACHR-WX9 5.4.0-92-generic #103-Ubuntu SMP Fri Nov 26 16:13:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  13:41:16 up  1:26,  0 users,  load average: 0.86, 0.58, 0.43 886b1acca562f2e1f8282af8273d2f63a2a8dc2e

--- nexus-am/am/src/riscv32/nemu/cte.c
+++ nexus-am/am/src/riscv32/nemu/cte.c
@@ -8,7 +8,8 @@ _Context* __am_irq_handle(_Context *c) {
   if (user_handler) {
     _Event ev = {0};
     switch (c->cause) {
-      default: ev.event = _EVENT_ERROR; break;
+      default: printf("casue %d\n",c->cause);
+      ev.event = _EVENT_ERROR; break;
     }
 
     next = user_handler(ev, c);
