5fd81201d5404f6416e9acd839bf5332b7d1e439
tracer-ics2019 <tracer@njuics.org>
2022-01-08 19:50:34 -0800

    >  run U201811329 xie Linux xie-MACHR-WX9 5.4.0-92-generic #103-Ubuntu SMP Fri Nov 26 16:13:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  19:50:34 up 7 min,  0 users,  load average: 0.49, 0.48, 0.29 828bd521dcd99c844c028a5a5d27444ed0b5bb

--- nexus-am/am/src/nemu-common/nemu-timer.c
+++ nexus-am/am/src/nemu-common/nemu-timer.c
@@ -6,10 +6,8 @@ size_t __am_timer_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_TIMER_UPTIME: {
       uint32_t t = 0;
-      printf("111\n");
       _DEV_TIMER_UPTIME_t *uptime = (_DEV_TIMER_UPTIME_t *)buf;
       t = inl(RTC_ADDR);
-      printf("222\n");
       if(uptime->lo > t){
         uptime->hi +=1;
       }
