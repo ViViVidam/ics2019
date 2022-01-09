a51ef399b198cd4aeb6a0e3420d7e3031a3280fc
tracer-ics2019 <tracer@njuics.org>
2022-01-09 12:19:55 -0800

    >  run U201811329 xie Linux xie-MACHR-WX9 5.4.0-92-generic #103-Ubuntu SMP Fri Nov 26 16:13:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  12:19:55 up 16:36,  0 users,  load average: 0.35, 0.21, 0.31 419b1640d1f85b055b4cf46ce54bba529155d7c1

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -12,34 +12,27 @@ extern uint8_t ramdisk_start;
 extern end;
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  Elf_Ehdr header;
-  Elf_Phdr segment;
-  Elf32_Addr vaddr;
-  Elf32_Word filesize;
-  Elf32_Word memsize;
-  Elf32_Off segmentoffset = 0;
-  Elf32_Off segcontent_off = 0;
-  ramdisk_read(&header, 0, sizeof(Elf32_Ehdr));
-  segmentoffset = header.e_phoff;
-  for(int i=0;i<header.e_phnum;i++){
-    ramdisk_read(&segment,header.e_phoff+i*header.e_phentsize,header.e_phentsize);
-    if(segment.p_type==PT_LOAD){
-      memcpy(segment.p_vaddr,&ramdisk_start+segment.p_offset,segment.p_filesz);
-      memset(segment.p_vaddr+segment.p_filesz,0,segment.p_memsz-segment.p_filesz);
+  Elf_Ehdr head;
+  int fd=fs_open(filename,0,0);
+  fs_lseek(fd,0,SEEK_SET);
+  fs_read(fd,&head,sizeof(head));
+  for(int i=0;i<head.e_phnum;i++){
+    Elf_Phdr temp;
+    fs_lseek(fd,head.e_phoff+i*head.e_phentsize,SEEK_SET);
+    fs_read(fd,&temp,sizeof(temp));
+    if(temp.p_type==PT_LOAD){
+      fs_lseek(fd,temp.p_offset,SEEK_SET);
+      fs_read(fd,(void*)temp.p_vaddr,temp.p_filesz);
+      memset((void*)(temp.p_vaddr+temp.p_filesz),0,temp.p_memsz-temp.p_filesz);
     }
   }
-  printf("%x\n",&end);
-  return header.e_entry;
-  //retrun NULL;
+  return head.e_entry;
 }
 
+//finished naive_uload
 void naive_uload(PCB *pcb, const char *filename) {
-  printf("naive load\n");
   uintptr_t entry = loader(pcb, filename);
   Log("Jump to entry = %x", entry);
-  if(entry%4!=0){
-    printf("invalid addr\n");
-  }
   ((void(*)())entry) ();
 }
 
