2b9b755c54e546a4a0d72e832167729aefcd04c0
tracer-ics2019 <tracer@njuics.org>
2022-01-09 00:01:26 -0800

    >  run U201811329 xie Linux xie-MACHR-WX9 5.4.0-92-generic #103-Ubuntu SMP Fri Nov 26 16:13:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  00:01:26 up  4:18,  0 users,  load average: 0.57, 0.56, 0.38 73a5f1a2b12cef7892fcb7f3a38cbd5cc75060b5

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -72,10 +72,10 @@ int _write(int fd, void *buf, size_t count) {
 void *_sbrk(intptr_t increment) {
   static void* program_break=(uintptr_t)&_end;
   void* old=program_break;
-  if(_syscall_(SYS_brk,(uintptr_t)program_break+increment,0,0)==0){
+  //if(_syscall_(SYS_brk,(uintptr_t)program_break+increment,0,0)==0){
     program_break+=increment;
     return(void*)old;
-  }
+  //}
   _syscall_(SYS_write,1,buffer,32);
   return (void *)-1;
 }
