dcc3e28653d6f0e3b5a2fe0fe8e87fc328b48046
tracer-ics2019 <tracer@njuics.org>
2022-01-09 12:29:54 -0800

    >  run U201811329 xie Linux xie-MACHR-WX9 5.4.0-92-generic #103-Ubuntu SMP Fri Nov 26 16:13:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  12:29:54 up 16:46,  0 users,  load average: 0.52, 0.51, 0.43 a1b0014389c9d9cd83dcf6d8583bf3abfc3e81c9

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -24,7 +24,7 @@ void init_proc() {
   Log("Initializing processes...");
 
   // load program here
-  naive_uload(NULL, "/bin/hello");
+  naive_uload(NULL, "/bin/txt");
 }
 
 _Context* schedule(_Context *prev) {
