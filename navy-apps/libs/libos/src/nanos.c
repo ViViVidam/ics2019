66bbeae9c1d7e45993b73a5cebecde6b47383c2b
tracer-ics2019 <tracer@njuics.org>
2022-01-08 23:19:38 -0800

    >  run U201811329 xie Linux xie-MACHR-WX9 5.4.0-92-generic #103-Ubuntu SMP Fri Nov 26 16:13:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  23:19:38 up  3:36,  0 users,  load average: 0.36, 0.34, 0.36 b013b30341c8b6aaf3e8d5ee7977066ef70b6550

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -61,7 +61,7 @@ int _open(const char *path, int flags, mode_t mode) {
 }
 
 int _write(int fd, void *buf, size_t count) {
-  _syscall_(SYS_write,1,buffer,32);
+  //_syscall_(SYS_write,1,buffer,32);
   return _syscall_(SYS_write,fd,buf,count);
 }
 
