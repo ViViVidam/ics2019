f81de192e7dbfebf0db5e69bca9ec202f467177d
tracer-ics2019 <tracer@njuics.org>
2022-01-09 01:15:17 -0800

    >  compile U201811329 xie Linux xie-MACHR-WX9 5.4.0-92-generic #103-Ubuntu SMP Fri Nov 26 16:13:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  01:15:17 up  5:32,  0 users,  load average: 0.59, 0.84, 0.63 8f23ddcdb632c508f58d63cc1e997956e03e1d7

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -82,7 +82,8 @@ void init_difftest(char *ref_so_file, long img_size) {
 }
 
 static void checkregs(CPU_state *ref, vaddr_t pc) {
-  printf("pc:%x code:%x\n",pc,instr_fetch(pc, 4));
+  printf("%x\n",pc);
+  //printf("pc:%x code:%x\n",pc,instr_fetch(pc, 4));
   if (!isa_difftest_checkregs(ref, pc)) {
     extern void isa_reg_display(void);
     isa_reg_display();
