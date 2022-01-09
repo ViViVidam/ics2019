b48478534e59d380cfa833f065e4ed10f00d4442
tracer-ics2019 <tracer@njuics.org>
2022-01-08 22:40:31 -0800

    >  run U201811329 xie Linux xie-MACHR-WX9 5.4.0-92-generic #103-Ubuntu SMP Fri Nov 26 16:13:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  22:40:31 up  2:57,  0 users,  load average: 0.78, 0.64, 0.54 f625d94012fbc7e1877c8b384172ebbd49a8c2e2

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -13,7 +13,8 @@ int main() {
   printf("%s", logo);
   Log("'Hello World!' from Nanos-lite");
   Log("Build time: %s, %s", __TIME__, __DATE__);
-
+  printf("222\n");
+  Log("222");
 #ifdef HAS_VME
   init_mm();
 #endif
@@ -29,8 +30,7 @@ int main() {
   init_fs();
 
   init_proc();
-  printf("222\n");
-  Log("222");
+  
   Log("Finish initialization");
 #ifdef HAS_CTE
   _yield();
