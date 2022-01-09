47adb4010fe1337351622afee9f5e5ac4653608f
tracer-ics2019 <tracer@njuics.org>
2022-01-09 01:09:57 -0800

    >  compile U201811329 xie Linux xie-MACHR-WX9 5.4.0-92-generic #103-Ubuntu SMP Fri Nov 26 16:13:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  01:09:57 up  5:26,  0 users,  load average: 0.60, 0.51, 0.42 693d5629a8b72aa5dd41c3cfbf33bfdbe918133

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -2,7 +2,7 @@
 
 #include "nemu.h"
 #include "monitor/monitor.h"
-
+#include "cpu/exec.h"
 void (*ref_difftest_memcpy_from_dut)(paddr_t dest, void *src, size_t n) = NULL;
 void (*ref_difftest_getregs)(void *c) = NULL;
 void (*ref_difftest_setregs)(const void *c) = NULL;
@@ -84,7 +84,6 @@ void init_difftest(char *ref_so_file, long img_size) {
 static void checkregs(CPU_state *ref, vaddr_t pc) {
   if (!isa_difftest_checkregs(ref, pc)) {
     extern void isa_reg_display(void);
-    extern uint32_t instr_fetch(vaddr_t,int);
     printf("pc:%x code:%x\n",pc,instr_fetch(pc, 4));
     isa_reg_display();
     nemu_state.state = NEMU_ABORT;
