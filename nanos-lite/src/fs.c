160085175d2319a30d24691f64e4b9060e6bb0f5
tracer-ics2019 <tracer@njuics.org>
2022-01-09 21:59:46 -0800

    >  run U201811329 xie Linux xie-MACHR-WX9 5.4.0-92-generic #103-Ubuntu SMP Fri Nov 26 16:13:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  21:59:46 up  5:27,  0 users,  load average: 0.44, 0.38, 0.37 ffa4bf439474768d152720c74937f28783c497a5

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -63,7 +63,7 @@ size_t fs_read(int fd, void *buf, size_t len){
     len=file_table[fd].size-file_table[fd].open_offset;
   if(len<0)
     len=0;
-  if(file_table[fd].read==NULL||file_table[fd].read!=invalid_read)
+  if(file_table[fd].read==NULL)
     ramdisk_read(buf,file_table[fd].disk_offset+file_table[fd].open_offset,len);
   else
     len=file_table[fd].read(buf,file_table[fd].disk_offset+file_table[fd].open_offset,len);
@@ -84,7 +84,7 @@ size_t fs_write(int fd, const void *buf, size_t len){
   }*/
   if(file_table[fd].open_offset+len>=file_table[fd].size)
     len=file_table[fd].size-file_table[fd].open_offset;
-  if(file_table[fd].write==NULL || file_table[fd].write==invalid_write)
+  if(file_table[fd].write==NULL)
     length=ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
   else
     length=file_table[fd].write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
