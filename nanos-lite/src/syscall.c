a55e203852cad9bd340e4c8c2d5e949491da645c
tracer-ics2019 <tracer@njuics.org>
2022-01-09 18:30:17 -0800

    >  run U201811329 xie Linux xie-MACHR-WX9 5.4.0-92-generic #103-Ubuntu SMP Fri Nov 26 16:13:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  18:30:17 up  1:57,  0 users,  load average: 0.59, 0.52, 0.46 8ad2b4689924779c94eaf923c8c2848fd4fae58c

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -9,7 +9,7 @@ _Context* do_syscall(_Context *c) {
   a[2] = c->GPR3;
   a[3] = c->GPR4;
   int res=0;
-  if(a[0]!=9 | a[0]!=4){
+  if(a[0]!=9){
     panic("Unhandled syscall ID = %d %d", a[0],SYS_open);
   }
   switch (a[0]) {
