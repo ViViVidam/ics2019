adb5b4bc8706caa62c3ce2df3d50b46b3495af0c
tracer-ics2019 <tracer@njuics.org>
2022-01-09 22:00:17 -0800

    >  run U201811329 xie Linux xie-MACHR-WX9 5.4.0-92-generic #103-Ubuntu SMP Fri Nov 26 16:13:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  22:00:17 up  5:27,  0 users,  load average: 0.27, 0.35, 0.36 57d09378413c2327aa82e89ed78a0a573eaea2

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -32,6 +32,7 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
   return 0;
 }
 
+/* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
   {"stdin", 0, 0, invalid_read, invalid_write},
   {"stdout", 0, 0, invalid_read, serial_write},
@@ -76,19 +77,21 @@ int fs_close(int fd){
 }
 size_t fs_write(int fd, const void *buf, size_t len){
   size_t length=0;
-  /*if(fd==1||fd==2){
+  if(fd==1||fd==2){
     for(int i=0;i<len;i++){
       _putc(((char *)buf)[i]);
     }
     length=len;
-  }*/
+  }
   if(file_table[fd].open_offset+len>=file_table[fd].size)
     len=file_table[fd].size-file_table[fd].open_offset;
-  if(file_table[fd].write==NULL)
-    length=ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
-  else
-    length=file_table[fd].write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
-  file_table[fd].open_offset+=length;
+  if(fd>2){
+    if(file_table[fd].write==NULL)
+      length=ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+    else
+      length=file_table[fd].write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
+    file_table[fd].open_offset+=length;
+  }
   return length;
 }
 
