325158c646a48c2707e7d7c02297260f70d61270
tracer-ics2019 <tracer@njuics.org>
2022-01-10 10:17:17 -0800

    >  run U201714670 xie Linux xie-MACHR-WX9 5.4.0-92-generic #103-Ubuntu SMP Fri Nov 26 16:13:00 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  10:17:17 up 7 min,  0 users,  load average: 0.44, 0.74, 0.47 d50c1c00694ed5fcd2608a1ba0453e0b6cdabdc4

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -2,36 +2,39 @@
 #include <amdev.h>
 #include <nemu.h>
 
+void draw_sync();
+int screen_width();
+int screen_height();
+
 size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_INFO: {
-      uint32_t inf = 0;
       _DEV_VIDEO_INFO_t *info = (_DEV_VIDEO_INFO_t *)buf;
-      inf = inl(SCREEN_ADDR);
-      info->width = inf>>16;
-      info->height = inf&65535;
+      info->width = 400;
+      info->height = 300;
       return sizeof(_DEV_VIDEO_INFO_t);
     }
   }
   return 0;
 }
-//int x, y; uint32_t *pixels; int w, h, sync
+
 size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
-      int i = 0,j = 0;
-      int W = screen_width(), H = screen_height();
-      uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
       _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
-      for(i=0;i<ctl->w;i++){
-        for(j=0;j<ctl->h;j++){
-          fb[(ctl->y+j)*W+ctl->x+i]=*(ctl->pixels+j*(ctl->w)+i);
+      int x=ctl->x,y=ctl->y,h=ctl->h,w=ctl->w;
+      int W=screen_width();
+      int H=screen_height();
+      uint32_t *pixels=ctl->pixels;
+      uint32_t *fb=(uint32_t *)(uintptr_t)FB_ADDR;
+      for(int i=0;i<h;i++){
+        for(int j=0;j<w;j++){
+          fb[(y+i)*W+x+j]=pixels[i*w+j];
         }
       }
       if (ctl->sync) {
         outl(SYNC_ADDR, 0);
       }
-      //printf("(%d,%d) (%d,%d)\n",ctl->x,ctl->y,ctl->w,ctl->h);
       return size;
     }
   }
@@ -42,7 +45,6 @@ void __am_vga_init() {
   int i;
   int size = screen_width() * screen_height();
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < size; i ++) fb[i] = i;
-  //printf("done1\n");
+  for (i = 0; i < size; i ++) fb[i] = 0;
   draw_sync();
 }
